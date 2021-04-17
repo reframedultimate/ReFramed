ea0b63bc188525733105be40527c01b0b3541aae
TheComet <alex.murray@gmx.ch>
2021-04-17 17:46:33 +0200

    Adding video player (stub)

---
+++ frontend/uh/src/views/VideoPlayer.cpp
@@ -0,0 +1,17 @@
+#include "uh/views/VideoPlayer.hpp"
+#include <QVBoxLayout>
+
+namespace uh {
+
+// ----------------------------------------------------------------------------
+VideoPlayer::VideoPlayer(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+// ----------------------------------------------------------------------------
+VideoPlayer::~VideoPlayer()
+{
+}
+
+}
