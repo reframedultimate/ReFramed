b6117f7d4525a3f007ad004815f08a1c42e99526
TheComet <alex.murray@gmx.ch>
2021-04-23 23:01:47 +0200

    Adding debug count to RefCounted objects

--- frontend/uh/include/uh/RefCounted.hpp
+++ frontend/uh/include/uh/RefCounted.hpp
@@ -2,27 +2,39 @@
 
 #include "uh/config.hpp"
 
+#ifndef DEBUG
+#   include <atomic>
+#endif
+
 namespace uh {
 
 class UH_PUBLIC_API RefCounted
 {
 public:
-	RefCounted();
-	virtual ~RefCounted();
+    RefCounted();
+    virtual ~RefCounted();
 
-	RefCounted(const RefCounted&) = delete;
-	RefCounted& operator=(const RefCounted&) = delete;
+    RefCounted(const RefCounted&) = delete;
+    RefCounted& operator=(const RefCounted&) = delete;
 
-	void incRef();
-	void decRef();
+    void incRef();
+    void decRef();
 
-	int refs() const { return refs_; }
+    int refs() const { return refs_; }
+
+#ifndef DEBUG
+    static int total() { return total_.load(); }
+#endif
 
 protected:
-	virtual void seppuku();
+    virtual void seppuku();
 
 private:
-	int refs_;
+    int refs_;
+
+#ifndef DEBUG
+    static std::atomic<int> total_;
+#endif
 };
 
 }
