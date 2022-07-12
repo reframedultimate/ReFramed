f34abae66a13593d47e72b73588844e3c2dd7368
TheComet <alex.murray@gmx.ch>
2022-07-13 01:42:35 +0200

    Session subclasses

--- rfcommon/include/rfcommon/Session.hpp
+++ rfcommon/include/rfcommon/Session.hpp
@@ -4,15 +4,18 @@
 
 #include "rfcommon/config.hpp"
 #include "rfcommon/Types.hpp"
-#include "rfcommon/MappingInfo.hpp"
-#include "rfcommon/RefCounted.hpp"
+#include "rfcommon/Reference.hpp"
 #include "rfcommon/ListenerDispatcher.hpp"
 #include "rfcommon/Frame.hpp"
+#include "rfcommon/Vector.hpp"
 
 namespace rfcommon {
 
-class FighterState;
+class MappingInfo;
 class SessionListener;
+class SessionMetaData;
+class GameSessionMetaData;
+class TrainingSessionMetaData;
 
 class RFCOMMON_PUBLIC_API Session : public RefCounted
 {
@@ -20,14 +23,17 @@ public:
     Session();
     virtual ~Session();
 
+    static Session* load(const String& fileName);
     bool save(const String& fileName);
 
     /*!
      * \brief Returns information on how to map fighter/stage/state IDs to
      * strings.
      */
-    const MappingInfo& mappingInfo() const
+    MappingInfo* mappingInfo() const
         { return mappingInfo_; }
+
+    virtual SessionMetaData* metaData() const = 0;
 
     int frameCount() const
         { return frames_.count(); }
@@ -36,13 +42,23 @@ public:
         { return frames_[frameIdx]; }
 
     const Frame& firstFrame() const
-        { assert(frameCount() > 0); return frames_.front(); }
+    {
+        assert(frameCount() > 0);
+        return frames_.front();
+    }
 
     const Frame& lastFrame() const
-        { assert(frameCount() > 0); return frames_.back(); }
+    {
+        assert(frameCount() > 0);
+        return frames_.back();
+    }
 
     const FighterState& state(int frameIdx, int fighterIdx) const
-        { assert(fighterIdx < fighterCount()); assert(frameIdx < frameCount()); return frames_[frameIdx].fighter(fighterIdx); }
+    {
+        assert(fighterIdx < fighterCount());
+        assert(frameIdx < frameCount());
+        return frames_[frameIdx].fighter(fighterIdx);
+    }
 
     void addFrame(Frame&& frame);
 
@@ -51,8 +67,33 @@ public:
 protected:
     int findWinner() const;
 
-protected:
+private:
+    Reference<MappingInfo> mappingInfo_;
     Vector<Frame> frames_;
+};
+
+class RFCOMMON_PUBLIC_API GameSession : public Session
+{
+public:
+    GameSessionMetaData* gameMetaData() const
+        { return metaData_; }
+
+    SessionMetaData* metaData() const override;
+
+private:
+    Reference<GameSessionMetaData> metaData_;
+};
+
+class RFCOMMON_PUBLIC_API TrainingSession : public Session
+{
+public:
+    TrainingSessionMetaData* gameMetaData() const
+        { return metaData_; }
+
+    SessionMetaData* metaData() const override;
+
+private:
+    Reference<TrainingSessionMetaData> metaData_;
 };
 
 }
