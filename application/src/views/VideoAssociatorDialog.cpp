4448633538b5da8942cba3f479b57cb19bfead4a
TheComet <alex.murray@gmx.ch>
2022-08-12 23:33:28 +0200

    Show error if video plugin couldn't be found in video associator

--- application/src/views/VideoAssociatorDialog.cpp
+++ application/src/views/VideoAssociatorDialog.cpp
@@ -26,7 +26,8 @@ VideoAssociatorDialog::VideoAssociatorDialog(
 {
     ui_->setupUi(this);
 
-    for (const auto& factoryName : pluginManager_->availableFactoryNames(RFPluginType::UI | RFPluginType::VIDEO_PLAYER))
+    const auto availableVideoPlugins = pluginManager_->availableFactoryNames(RFPluginType::UI | RFPluginType::VIDEO_PLAYER);
+    for (const auto& factoryName : availableVideoPlugins)
     {
         videoPlugin_ = pluginManager_->create(factoryName);
         if (videoPlugin_)
@@ -41,6 +42,16 @@ VideoAssociatorDialog::VideoAssociatorDialog(
 
     if (videoView_)
         ui_->layout_videoPlugin->addWidget(videoView_);
+    else
+    {
+        QLabel* label = new QLabel;
+        if (availableVideoPlugins.size() == 0)
+            label->setText("No video plugin was found! Make sure you enabled the built-in video plugin, and if you built from source, ensure that you specified -DREFRAMED_plugin-video-player:BOOL=ON");
+        else
+            label->setText("Failed to create video player view!");
+        label->setWordWrap(true);
+        ui_->layout_videoPlugin->addWidget(label);
+    }
 
     connect(ui_->pushButton_cancel, &QPushButton::released, this, &VideoAssociatorDialog::close);
 }
